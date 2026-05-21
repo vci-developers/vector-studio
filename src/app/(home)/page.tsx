283d96b252a39d6a5a1816e46d316c6bd3273939
Aryaman Shodhan <ashodha1@jhu.edu>
2026-05-21 01:25:43 -0400

    Resolve prettier formatting

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,5 @@
 import LogoutButton from '@/components/auth-session/logout-button';
 
 export default function Home() {
-    return (
-        <LogoutButton />
-    );
+    return <LogoutButton />;
 }
